17fc05ddeff93ceeb3bcdf9ef37ee279844cb7b0
unknown <hambalijoy9@gmail.com>
2022-06-27 22:08:21 +0700

    portfolio tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+  container: {
+center: true,
+pndding: '16px',
+  } , 
+    extend: {
+      colors: {
+        primary: '#38bdf8',
+        secondary: '#64748b',
+        dark: '#0f172a',
+      },
+      screens:{
+        '2xl' : '1230px',
+      },
+    },
+  },
+  plugins: [],
+};
